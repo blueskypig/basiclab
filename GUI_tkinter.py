cda4d97fc1a1c3d69cf41d543c3dcecb917ad81c
noobsmonster <12567356+blueskypig@users.noreply.github.com>
2018-05-29 01:09:36 -0700

    update

--- GUI_tkinter.py
+++ GUI_tkinter.py
@@ -3,23 +3,26 @@ from PIL import Image, ImageTk
 
 
 def showimge(event):
-    print("Single Click, Button-l")
+	top = Toplevel()
+	image = Image.open('img.jpg')
+	photo = ImageTk.PhotoImage(image)
+	canvas = Canvas(top, width = image.width, height = image.height, bg = 'white')
+	canvas.create_image(0, 0, image = photo, anchor = 'nw')
+	canvas.pack()
+	top.mainloop()
 
 
 root = Tk()  #create object of window
 root.title('应用程序窗口')
 root.resizable(False, False)
-windowWidth = 800
-windowHeight = 800
+windowWidth = 400
+windowHeight = 400
 screenWidth, screenHeight = root.maxsize()
 geotryParam = '%dx%d+%d+%d' % (windowWidth, windowHeight, (screenWidth-windowWidth)/2, (screenHeight-windowHeight)/2)
 root.geometry(geotryParam) #窗口大小以及偏移坐标
 root.wm_attributes('-topmost', 1)
 
-image = Image.open('img.jpg')
-photo = ImageTk.PhotoImage(image)
-#label_img = Label(root, image=photo)
-#label_img.pack()
+
 #label_text = Label(root, text='显示照片')
 #label_text.pack()
 
